639e27b9914f0a7e31b36de90a942c0d2005843c
FEIFEI <2350099033@qq.com>
2023-11-10 21:46:59 +0800

    feat: Recommend.vue       RecommendDetail.vue       RecommendPage.vue

--- src/components/Recommend.vue
+++ src/components/Recommend.vue
@@ -1,97 +1,122 @@
 <script setup lang="ts">
 
 import router from "../ts/router.ts";
-import {ref} from "vue";
+import {ref, watch} from "vue";
 import {showFailToast, showSuccessToast} from "vant";
 import axios from "../ts/axios";
 import {Product} from "../ts/model.ts";
 
 
 const list = ref<Product[]>([
-    {
-        "id": 0,
-        "name": "string",
-        "details": "string",
-        "price": 0,
-        "antiRisk": 0,
-        "flexibility": 0,
-        "returnRate": 0,
-        "state": 0
-    }
+  {
+    "id": 0,
+    "name": "string",
+    "details": "string",
+    "price": 0,
+    "antiRisk": 0,
+    "flexibility": 0,
+    "returnRate": 0,
+    "state": 0
+  }
 ],)
 
 const initComponent = async () => {
-    let indicator = [
-        "Comprehensive",
-        "Comprehensive",
-        "Comprehensive",
-        "Comprehensive"
-    ]
-    let res = await axios.get(`/product/sortBy${indicator[props.indicator]}`)
+  let indicator = [
+    "Comprehensive",
+    "Return",
+    "Flexibility",
+    "Risk"
+  ]
+  let res = await axios.get(`/product/sortBy${indicator[props.indicator]}`)
 
-    if (!res.data["code"]) {
-        showFailToast(res.data["msg"])
-        return
-    }
+  if (!res.data["code"]) {
+    showFailToast(res.data["msg"])
+    return
+  }
 
-    if (props.indicator == "0") {
-        list.value.splice(0, list.value.length)
-        res.data.data.forEach((item) => {
-            list.value.push(item["product"])
-        })
-    } else {
-        list.value = res.data["data"]
-    }
+  if (props.indicator == "0") {
+    list.value.splice(0, list.value.length)
+    res.data.data.forEach((item) => {
+      list.value.push(item["product"])
+    })
+  } else {
+    list.value = res.data["data"]
+  }
 
 }
+const add = async (index: number, productId: number) => {//添加收藏，调用时候用@click="add(index, item.id)"
+  try {
+    const res = await axios.post('/product/changeFavState', {
+      id: productId,
+      name: list.value[index].name,
+      details: list.value[index].details,
+      price: list.value[index].price,
+      antiRisk: list.value[index].antiRisk,
+      flexibility: list.value[index].flexibility,
+      returnRate: list.value[index].returnRate,
+      state: list.value[index].state
+    });
+    if (res.data.code === 200) {
+      alert('收藏成功！');
+
+    } else {
+      alert(res.data.msg);
+    }
+  } catch (error) {
+    console.error('Error adding to favorites:', error);
+    showFailToast('收藏失败！');
+  }
+}
 initComponent()
+
+watch(() => props.indicator, (newIndicator, oldIndicator) => {
+  if (newIndicator !== oldIndicator) {
+    initComponent();
+  }
+});
 
 const props = defineProps(["indicator"])
 </script>
 
 <template>
-    <div style="margin-bottom: 3rem">
-        <div v-for="(item, index) in list">
-            <van-row justify="center" align="center">
-                <van-col span="20">
-                    <van-card
+  <div style="margin-bottom: 3rem">
+    <div v-for="(item, index) in list">
+      <van-row justify="center" align="center">
+        <van-col span="20">
+          <van-card
+              :price="item.price.toFixed(2)"
+              :title="item.name"
+              :desc="item.details"
+              @click="router.push(`/recommend/detail/${item.id}`)"
+          >
+          </van-card>
+        </van-col>
+        <van-col span="4">
+          <van-button
+              color="linear-gradient(to right, #81CAFE, #0396ff)"
+              @click="showSuccessToast('收藏成功！')"
 
-                            :title="item.name"
-                            :desc="item.details"
-                            @click="router.push(`/recommend/detail/${item.id}`)"
-                    >
-                        <template #price>
-                            <h2 style="color: orangered">
-                                <strong>￥{{ item.price.toFixed(2) }}
-                                </strong></h2>
-                        </template>
-                    </van-card>
-                </van-col>
-                <van-col span="4">
-                    <van-button
-                            color="linear-gradient(to right, #81CAFE, #0396ff)"
-                            @click="showSuccessToast('收藏成功！')"
-                            block
-                            size="large"
-                    >
-                        <template #default>
-                            <van-icon name="plus"/>
-                            <br/>
-                            添加收藏
-                        </template>
-                    </van-button>
-                </van-col>
-            </van-row>
+              block
+              size="large"
+          >
+            <template #default>
+              <van-icon name="plus"/>
+              <br/>
+              添加收藏
+            </template>
+          </van-button>
+        </van-col>
+      </van-row>
 
-        </div>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
     </div>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+  </div>
 </template>
 
 <style scoped>
--- src/pages/mainLayout/RecommendPage.vue
+++ src/pages/mainLayout/RecommendPage.vue
@@ -7,30 +7,30 @@ const activeIndicator = ref("0")
 </script>
 
 <template>
-    <van-search
-            placeholder="请输入搜索关键词"
-            input-align="center"
-    />
-    <van-tabs
-            v-model:active="activeIndicator"
-    >
-        <van-tab title="综合" name="0"></van-tab>
-        <van-tab title="属性1" name="1"></van-tab>
-        <van-tab title="属性2" name="2"></van-tab>
-        <van-tab title="属性3" name="3"></van-tab>
-    </van-tabs>
-    <recommend style="margin-bottom:3rem" :indicator="activeIndicator"/>
-	<!--    -->
-	<!--    <p>没有符合需求的？-->
-	<!--        <van-button type="primary" block-->
-	<!--                                  @click="router.push('/riskConfig')">-->
-	<!--        点我调整风险配置</van-button>-->
-	<!--    </p>-->
-	<!--    <br />-->
-	<!--    <br />-->
-	<!--    <br />-->
-	<!--    <br />-->
-	<!--    <br />-->
+  <van-search
+      placeholder="请输入搜索关键词"
+      input-align="center"
+  />
+  <van-tabs
+      v-model:active="activeIndicator"
+  >
+    <van-tab title="综合" name="0"></van-tab>
+    <van-tab title="收益率" name="1"></van-tab>
+    <van-tab title="灵活度" name="2"></van-tab>
+    <van-tab title="非风险性" name="3"></van-tab>
+  </van-tabs>
+  <recommend style="margin-bottom:3rem" :indicator="activeIndicator"/>
+  <!--    -->
+  <!--    <p>没有符合需求的？-->
+  <!--        <van-button type="primary" block-->
+  <!--                                  @click="router.push('/riskConfig')">-->
+  <!--        点我调整风险配置</van-button>-->
+  <!--    </p>-->
+  <!--    <br />-->
+  <!--    <br />-->
+  <!--    <br />-->
+  <!--    <br />-->
+  <!--    <br />-->
 </template>
 
 <style scoped>
