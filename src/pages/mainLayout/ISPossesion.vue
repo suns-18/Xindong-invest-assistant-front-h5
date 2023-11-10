99ac201c22168a8c65df49f431137034caa1cd8f
NBDatsuya <2646809050@qq.com>
2023-11-10 17:34:38 +0800

    feat: ISPossesion.vue: using api, tradeRecord

--- src/pages/mainLayout/ISPossesion.vue
+++ src/pages/mainLayout/ISPossesion.vue
@@ -6,6 +6,7 @@ import pinia from "../../ts/store.ts";
 import {computed, reactive, ref} from "vue";
 import {showFailToast, showSuccessToast} from "vant";
 import axios from "../../ts/axios.ts";
+import {localTime} from "../../ts/transcript.ts";
 
 /*const purchasedStore = usePurchasedStore(pinia)
 
@@ -42,8 +43,30 @@ const stat = ref({
     ]
 })
 
+const tradeRecordList = ref([
+    {
+        "id": 1,
+        "productId": 1,
+        "price": 1,
+        "amount": 1,
+        "dealTime": "2023-11-07T10:44:09.000+00:00",
+        "sold": 1
+    }
+])
+
 const goSell = () => {
     router.push('/investSimulation/sell')
+}
+
+const getRecord = async () => {
+    let res = await axios.get(`/tradeRecord/queryList`)
+
+    if (!res.data["code"]) {
+        showFailToast(res.data["msg"])
+        return
+    }
+    tradeRecordList.value.splice(0, tradeRecordList.value.length)
+    tradeRecordList.value = res.data["data"]
 }
 
 const getStat = async () => {
@@ -53,12 +76,13 @@ const getStat = async () => {
         showFailToast(res.data["msg"])
         return
     }
-
+    stat.value.list.splice(0, stat.value.list.length)
     stat.value = res.data["data"]
 }
 
 const initComponent = () => {
     getStat()
+    getRecord()
 }
 initComponent()
 
@@ -75,7 +99,7 @@ initComponent()
                         <van-icon name="eye-o"/>
                     </template>
                     <template #text>
-                        <span>{{ stat.totalAssets }}</span>
+                        <span>{{ stat.totalAssets.toFixed(2) }}</span>
                     </template>
                 </van-grid-item>
                 <van-grid-item>
@@ -83,7 +107,7 @@ initComponent()
                         <span>总市值</span>
                     </template>
                     <template #text>
-                        <span>{{ stat.totalCurrentPrice }}</span>
+                        <span>{{ stat.totalCurrentPrice.toFixed(2) }}</span>
                     </template>
                 </van-grid-item>
                 <van-grid-item>
@@ -91,57 +115,87 @@ initComponent()
                         <span>当日参考盈亏</span>
                     </template>
                     <template #text>
-                        <span>{{ stat.dailyProfit }}</span>
+                        <span>{{ stat.dailyProfit.toFixed(2) }}</span>
                     </template>
                 </van-grid-item>
             </van-grid>
             <br/>
+            <!--            <table>
+														<thead>
+														<tr>
+																<th>产品名</th>
+																<th>盈亏</th>
+																<th>持仓</th>
+																<th>成本</th>
+																<th>现价</th>
+																<th>购买时间</th>
+																<th>操作</th>
+														</tr>
+														</thead>
+														<tbody>
+														<tr v-for="(item,index) in stat.list">
+																<td>{{ item.product.name }}</td>
+																<td>
+																		<p>￥{{
+																				((item.product.price - item.purchasePrice)
+																					* item.amount).toFixed(2)
+																				}}</p>
+																		<p>{{
+																				((item.product.price - item.purchasePrice)
+																					/ item.product.price).toFixed(2)
+																				}}%</p>
+																</td>
+																<td>{{ item.amount }}</td>
+																<td>{{ item.purchasePrice }}</td>
+																<td>{{ item.product.price }}</td>
+																<td>{{ item.purchaseDate }}</td>
+																<td>
+																		<van-button type="danger"
+																								size="large"
+																								@click="goSell">
+																				<van-icon name="bill"/>
+																				卖出
+																		</van-button>
+																</td>
+														</tr>
+														</tbody>
+												</table>-->
+        </van-tab>
+        <van-tab title="交易记录">
             <table>
                 <thead>
                 <tr>
-                    <th>产品名</th>
-                    <th>盈亏</th>
-                    <th>持仓</th>
-                    <th>成本</th>
-                    <th>现价</th>
+                    <th>产品名<br/>代码</th>
+                    <th>交易价格<br/>交易数</th>
+                    <th>总计</th>
                     <th>购买时间</th>
                     <th>操作</th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr v-for="(item,index) in stat.list">
-                    <td>{{ item.product.name }}</td>
-                    <td>
-                        <p>￥{{
-														((item.product.price - item.purchasePrice)
-															* item.amount).toFixed(2)
-                            }}</p>
-                        <p>{{
-														((item.product.price - item.purchasePrice)
-															/ item.product.price).toFixed(2)
-                            }}%</p>
+                <tr v-for="(item,index) in tradeRecordList">
+                    <td><p>{{ item.productId }}</p>
+                        <p> {{ item.productId }}</p>
                     </td>
-                    <td>{{ item.amount }}</td>
-                    <td>{{ item.purchasePrice }}</td>
-                    <td>{{ item.product.price }}</td>
-                    <td>{{ item.purchaseDate }}</td>
+                    <td><p>￥{{ item.price.toFixed(2) }}</p>
+
+                        <p>✖{{ item.amount }}</p></td>
+                    <td>￥{{ item.sold ? "" : "-" }}
+                        {{ (item.amount * item.price).toFixed(2) }}
+                    </td>
+                    <td>{{ localTime(item.dealTime) }}</td>
                     <td>
                         <van-button type="danger"
                                     size="large"
                                     @click="goSell">
                             <van-icon name="bill"/>
+                            <br/>
                             卖出
                         </van-button>
                     </td>
                 </tr>
                 </tbody>
             </table>
-        </van-tab>
-        <van-tab title="买入记录">
-
-        </van-tab>
-        <van-tab title="卖出记录">
-
         </van-tab>
     </van-tabs>
 </template>
