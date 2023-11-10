1e18a70ba6d412c566c3a2466c8c085835f1a20e
FEIFEI <2350099033@qq.com>
2023-11-10 15:31:39 +0800

    feat: Recommend.vue

--- src/components/Recommend.vue
+++ src/components/Recommend.vue
@@ -3,68 +3,69 @@
 import router from "../ts/router.ts";
 import {ref} from "vue";
 import {Product} from "../ts/model.ts";
-import {showSuccessToast} from "vant";
+import {showFailToast, showSuccessToast} from "vant";
+import axios from "../ts/axios";
 
-const list = ref<Product[]>([
-    {
-        id: 1,
-        name: "产品1",
-        description: "描述信息1",
-        price: "2.00",
-        thumb: "https://fastly.jsdelivr.net/npm/@vant/assets/ipad.jpeg",
-    }, {
-        id: 2,
-        name: "产品2",
-        description: "描述信息2",
-        price: "2.00",
-        thumb: "https://fastly.jsdelivr.net/npm/@vant/assets/ipad.jpeg",
-    }, {
-        id: 3,
-        name: "产品3",
-        description: "描述信息3",
-        price: "2.00",
-        thumb: "https://fastly.jsdelivr.net/npm/@vant/assets/ipad.jpeg",
-    }, {
-        id: 4,
-        name: "产品4",
-        description: "描述信息4",
-        price: "2.00",
-        thumb: "https://fastly.jsdelivr.net/npm/@vant/assets/ipad.jpeg",
+
+const list = ref([
+  {
+    "product": {
+      "id": 0,
+      "name": "string",
+      "details": "string",
+      "price": 0,
+      "antiRisk": 0,
+      "flexibility": 0,
+      "returnRate": 0,
+      "state": 0
     }
-])
+  }
+],)
+
+const initComponent = () => {
+  let res = await axios.get(`/product/sortByComprehensive`)
+
+  if(!res.data["code"]){
+    showFailToast(res.data["msg"])
+    return
+  }
+
+  list.value = res.data["data"]
+}
+initComponent()
 </script>
 
 <template>
-    <div style="margin-bottom: 3rem">
-        
-        <div v-for="(item, index) in list">
-            <van-row justify="center" align="center">
-                <van-col span="20">
-                    <van-card
-                      :price="item.price"
-                      :desc="item.description"
-                      :title="item.name"
-                      :thumb="item.thumb"
-                      @click="router.push('/recommend/detail')"
-                    >
-                    </van-card>
-                </van-col>
-                <van-col span="4">
-                    <van-button
-                      color="linear-gradient(to right, #81CAFE, #0396ff)"
-                      @click="showSuccessToast('收藏成功！')"
-                      block
-                      size="large"
-                    >
-                        <template #default>
-                            <van-icon name="plus"/><br/>
-                            添加收藏
-                        </template>
-                    </van-button>
-                </van-col>
-            </van-row>
-        </div>
+  <div style="margin-bottom: 3rem">
+
+    <div v-for="(item, index) in list">
+      <van-row justify="center" align="center">
+        <van-col span="20">
+          <van-card
+              :price="item.price"
+              :desc="item.description"
+              :title="item.name"
+              :thumb="item.thumb"
+              @click="router.push('/recommend/detail')"
+          >
+          </van-card>
+        </van-col>
+        <van-col span="4">
+          <van-button
+              color="linear-gradient(to right, #81CAFE, #0396ff)"
+              @click="showSuccessToast('收藏成功！')"
+              block
+              size="large"
+          >
+            <template #default>
+              <van-icon name="plus"/><br/>
+              添加收藏
+            </template>
+          </van-button>
+        </van-col>
+      </van-row>
     </div>
+  </div>
 </template>
 
 <style scoped>
